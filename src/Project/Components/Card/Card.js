3903343384b07c19c62b58ab802d112372412874
Lee Graham <graham.la3@gmail.com>
2018-07-09 13:09:11 -0600

    Specify card layouts for each type data filter

--- src/Project/Components/Card/Card.js
+++ src/Project/Components/Card/Card.js
@@ -4,33 +4,34 @@ const Card = (props) => {
   if(this.props.state.card.length === 39){
   return(
     <div>
-      <h2></h2>
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
+      <h2>{this.props.name}</h2>
+      <p>Model:{this.props.model}</p>
+      <p>Class:{this.props.class}</p>
+      <p># of Passengers:{this.props.passengers}</p>
+      <button>*</button>
     </div>
   )
 }
   if(this.props.state.card.length === 61) {
     return(
       <div>
-        <h2></h2>
-        <p></p>
-        <p></p>
-        <p></p>
-        <p></p>
+        <h2>{this.props.name}</h2>
+        <p>Terrain:{this.props.terrain}</p>
+        <p>Population:{this.props.population}</p>
+        <p>Climate:{this.props.climate}</p>
+        <p>Residents:{this.props.residents}</p>
+        <button>*</button>
       </div>
     )
   }
   if(this.props.state.card.length = 87)
   return(
     <div>
-      <h2></h2>
-      <p></p>
-      <p></p>
-      <p></p>
-      <p></p>
+      <h2>{this.props.name}</h2>
+      <p>Homeworld:{this.props.homeworld}</p>
+      <p>Species:{this.props.species}</p>
+      <p>Language:{this.props.language}</p>
+      <p>Population:{this.props.population}</p>
     </div>
   )
 }
